c55e7cb4a9baab26c22a518f6d890bac5497838d
LapiEmo <lapigerard@yahoo.fr>
2020-10-19 23:05:59 +0200

    partenaire

--- src/main/java/io/spiral/express/app/service/impl/PartenaireAppServiceImpl.java
+++ src/main/java/io/spiral/express/app/service/impl/PartenaireAppServiceImpl.java
@@ -4,12 +4,14 @@ import io.spiral.express.app.dto.PartenaireDTO;
 import io.spiral.express.app.repository.PartenaireAppRepository;
 import io.spiral.express.app.service.PartenaireAppService;
 import io.spiral.express.app.service.PersonneAppService;
+import io.spiral.express.app.service.error.ElementNonExistantException;
 import io.spiral.express.app.service.mapper.PartenaireMapper;
 import io.spiral.express.jhipster.domain.Partenaire;
 import io.spiral.express.jhipster.domain.Personne;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -45,12 +47,19 @@ public class PartenaireAppServiceImpl implements PartenaireAppService {
     @Override
     public PartenaireDTO findById(Long id) {
         log.info("Rechercher un partenaire via l'id: {}", id);
-        return null;
+        return partenaireAppRepository
+            .findById(id)
+            .map(partenaireMapper::toDto)
+            .orElseThrow(() -> new ElementNonExistantException("Pas de partenaire avec cet id: " + id));
     }
 
     @Override
     public Page<PartenaireDTO> findAll() {
         log.info("Rechercher tous les partenaire");
-        return null;
+        log.info("Rechercher tous les clients");
+        Pageable pageable = Pageable.unpaged();
+        return partenaireAppRepository
+            .findAll(pageable)
+            .map(partenaireMapper::toDto);
     }
 }
