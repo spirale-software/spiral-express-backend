5d7724eef7fdf72dd7a5775227e81277d7eff60c
LapiEmo <lapigerard@yahoo.fr>
2020-10-26 02:16:17 +0100

    nettoyage code

--- src/main/java/io/spiral/express/app/service/GenerationFicheEnvoiAppService.java
+++ src/main/java/io/spiral/express/app/service/GenerationFicheEnvoiAppService.java
@@ -1,7 +1,5 @@
 package io.spiral.express.app.service;
 
-import io.spiral.express.jhipster.domain.Envoi;
-
 public interface GenerationFicheEnvoiAppService {
 
     byte[] genererPdfAsByteArray(Long envoiId);
